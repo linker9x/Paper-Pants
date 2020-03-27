d7dce80c728116c2bd033aeccae1de5dd159855d
Unknown <lisa.winkler@stud.uni-hannover.de>
2020-03-27 16:50:02 -0400

    And then there were files

---
+++ paper_pants/technical_indicators/ohlcv_ti.py
@@ -0,0 +1,26 @@
+def macd():
+    return None
+
+
+def atr():
+    return None
+
+
+def rsi():
+    return None
+
+
+def adx():
+    return None
+
+
+def obv():
+    return None
+
+
+def slope():
+    return None
+
+
+def renko():
+    return None
